a0b6c81a6c6b26419e32058c90618d87895b334e
Brandon Reninger <brandonreninger@yahoo.com>
2020-05-14 16:11:57 -0600

    both gets for videogames work

--- Controllers/Models/VideoGame.cs
+++ Controllers/Models/VideoGame.cs
@@ -1,11 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace LibraryApi
 {
     public class VideoGame
     {
-        public string Title { set; get; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
         public string Platform { get; set; }
-        public string Rating { set; get; }
-        public decimal Price { set; private get; }
+        public string Rating { get; set; }
+        [Range(1, 100)]
+        public decimal Price { get; private set; }
+        public string Id { get; private set; }
+
+        public VideoGame()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
 
         public VideoGame(string title, string platform, string rating, decimal price)
         {
@@ -13,6 +25,7 @@ namespace LibraryApi
             Platform = platform;
             Rating = rating;
             Price = price;
+            Id = Guid.NewGuid().ToString();
         }
     }
 }
--- Controllers/VideoGamesController.cs
+++ Controllers/VideoGamesController.cs
@@ -4,8 +4,41 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryApi
 {
+
+    [ApiController]
+    [Route("api/[controller]")]
     public class VideoGamesController : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<IEnumerable<VideoGame>> GetAll()
+        {
+            try
+            {
+                return Ok(FakeDB.VideoGames);
+            }
+            catch (System.Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpGet("{videogameId}")]
+        public ActionResult<IEnumerable<VideoGame>> GetOne(string videogameId)
+        {
+            try
+            {
+                VideoGame foundVideoGame = FakeDB.VideoGames.Find(videogame => videogame.Id == videogameId);
+                if (foundVideoGame == null)
+                {
+                    throw new Exception("Invalid Id!");
+                }
+                return Ok(foundVideoGame);
+            }
+            catch (System.Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
 
     }
 }
